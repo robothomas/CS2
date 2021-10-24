14ef114029e854e6c22165ce10e604e9ec91e7ae
Dylan Thomas <thomas31@stolaf.edu>
2021-10-24 17:30:22 -0500

    HW 14 Submission

---
+++ hw14/class1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class class1 {
+    int var1;
+    bool var2;
+
+    public:
+        void init(int v1, bool v2) {
+            var1 = v1;
+            var2 = v2;
+        }
+};
---
+++ hw14/class2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class class2WithConstructor {
+    float var1;
+    short var2;
+
+    public: 
+        class2WithConstructor(float v1, short v2) {
+            var1 = v1;
+            var2 = v2;
+        }
+};
